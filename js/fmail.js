5dae18971ae2384518d06d4b8fef56e3d5e3e9ee
bhavikdabhi <bhavikdabhi1910@gmail.com>
2024-05-28 13:44:25 +0530

    use email.js

---
+++ js/fmail.js
@@ -0,0 +1,31 @@
+
+function send(){
+		
+    var params ={
+         name : document.getElementById("inputName").value,
+         lname : document.getElementById("inputLName").value,
+          email : document.getElementById("inputEmail").value,
+        subject : document.getElementById("inputRev").value,
+        rev : document.getElementById("inputSubject").value,
+         message : document.getElementById("inputMessage").value
+    }
+    emailjs.send("service_ptiym9a","template_x1axhxs",params).then(function(res){
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer:3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Mail successfully sent!"
+          });
+    })
+}
+
+
